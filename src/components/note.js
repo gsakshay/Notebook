dbc74544853969f31690ad1ad34d06edd59e6120
gsakshay <gsakshay20002002@gmail.com>
2020-05-25 21:25:18 +0530

    adding notes

--- src/components/note.js
+++ src/components/note.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function note () {
+function note (props) {
 	return (
 			<div className="note">
-				<h1>Note title</h1>
-				<p>Note content</p>
+				<h1>{props.title}</h1>
+				<p>{props.content}</p>
 			</div>
 		);
 }
--- src/container/App.js
+++ src/container/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Header from '../components/header';
 import Note from '../components/note';
 import Footer from '../components/footer';
+import notes from '../components/notes';
 
+const createNote = (note) =>{
+	return(
+	<Note 
+				title = {note.title}
+				content = {note.content}
+	/>
+	);
+} 
 
 function App(){
 	return(
 		<div>
 			<Header />
-			<Note />
+				{notes.map(createNote)}
 			<Footer />
 		</div>	
 		);
