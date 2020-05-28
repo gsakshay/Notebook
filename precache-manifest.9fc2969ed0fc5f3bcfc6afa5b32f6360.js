d4de08cdacab253e8f300e98abd1adc84a18c4b3
gsakshay <gsakshay20002002@gmail.com>
2020-05-28 18:12:52 +0530

    Updates

---
+++ precache-manifest.9fc2969ed0fc5f3bcfc6afa5b32f6360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f134357d385c4a45e9080531b3fa99",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f5a3144c6b5cf0a842da",
+    "url": "/my-app/static/css/main.99809021.chunk.css"
+  },
+  {
+    "revision": "c21dba7ec7092f7fd4d9",
+    "url": "/my-app/static/js/2.80286d81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.80286d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a3144c6b5cf0a842da",
+    "url": "/my-app/static/js/main.a9e7d548.chunk.js"
+  },
+  {
+    "revision": "cae40f5203be6914473f",
+    "url": "/my-app/static/js/runtime-main.251a0922.js"
+  }
+]);
